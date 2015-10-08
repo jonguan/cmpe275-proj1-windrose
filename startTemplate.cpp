727cc054f8e8e7ae3d1ef648eff28f76d1090287
Jon Guan <jonathan.guan@capitalone.com>
2015-10-08 11:55:56 -0700

    write functions bucketing data

--- startTemplate.cpp
+++ startTemplate.cpp
@@ -3,6 +3,32 @@
 // speed bucket function [5 buckets]
 // direction [divide 360 in number of sectors]
 
+// Divide speed into 5 buckets
+// Negative speeds are not allowed
+int speedBucket(double data) {
+	if (data < 0) return -1;
+
+	// Stratified divide
+	if (data < 1) {
+		return 0;
+	} else if (data < 3) {
+		return 1;
+	} else if (data < 5) {
+		return 2;
+	} else if (data < 7) {
+		return 3;
+	} else if (data < 9) {
+		return 4;
+	} else {
+		return 5;
+	}
+}
+
+// Divide 360 into 8 sectors
+int directionBucket(float degrees) {
+	int bucket = degrees / 8;
+	return bucket;
+}
 
 int main(...){
 	load file
